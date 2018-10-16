f166b3f5b56b49f146f72b1f43705031071274c2
tmonfared <tmonfared@af.local>
2018-10-16 11:54:28 -0700

    Add Hello world

---
+++ BasicRSQL.sql
@@ -0,0 +1,8 @@
+EXEC sp_execute_external_script
+  @language =N'R',
+  @script=N'OutputDataSet<-InputDataSet',
+  @input_data_1 =N'SELECT 1 AS hello'
+  WITH RESULT SETS (([Hello World] int));
+GO
+
+
